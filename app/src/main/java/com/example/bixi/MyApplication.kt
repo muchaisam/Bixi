a89099b901bb4ae40a1829bf5f0c7ac2f65b49a1
muchaisam <samson.muchai@strathmore.edu>
2024-03-05 17:32:05 +0300

    add myApplication to avoid firebase initialization errors

---
+++ app/src/main/java/com/example/bixi/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.example.bixi
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
